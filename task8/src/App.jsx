6c7ee0c0e78cd0ba50b35ec6254229cf9b16639d
Dasqin <ismayilov607@gmail.com>
2024-11-29 17:28:43 +0400

    changes

--- task8/src/App.jsx
+++ task8/src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Component } from 'react';
+import UserCv from './components/UserCV';
 
-const App = () => (
-  <div>
-    
-   
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="products">
+<UserCv/>
+      </div>
+    );
+  }
+}
+
 export default App;
-
-
-
