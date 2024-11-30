fdd36bf17fc209478ddeb97526394cd7a62812a0
Dasqin <ismayilov607@gmail.com>
2024-11-30 21:30:27 +0400

    changes

---
+++ MODUL4/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Wine from './components/.Wine'
+class App extends Component {
+  render() {
+    return (
+      <div className="products">
+      <Wine/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
---
+++ MODUL4/src/components/task2/Title.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+
+class Title extends Component {
+  name = "aksin";
+
+  clickHandler = (event) => {
+    console.log(this.name);
+    console.log(event.target);
+  }
+
+  render() {
+    const { title, rating } = this.props;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <span>{rating}</span>
+        <hr />
+        <button onClick={this.clickHandler}>Click me</button>
+      </div>
+    );
+  }
+}
+
+export default Title;
+
+
+
+
+
+
+
+
+
+
+
